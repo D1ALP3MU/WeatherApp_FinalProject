e1833e91f1c4b5356fdaeaaae42cbe333d4591e8
Diego Pérez <diegoalejandroperezm@gmail.com>
2023-11-21 20:39:21 -0500

    Se agregan iconos de clima e imagenes para fondo de la app

--- src/components/CardForecast.jsx
+++ src/components/CardForecast.jsx
@@ -1,11 +1,13 @@
 
 const CardForecast = ({forecast, initialValue, finalValue}) => {
+    // const {} = forecast;
 
     return (
         <>
             <div className='card'>
                 {forecast.list.slice(initialValue, finalValue).map((item, index) => (
-                    <div key={index} className='inner'> 
+                    <div key={index} className='inner'>
+                        <img src={`https://openweathermap.org/img/wn/${item.weather[0].icon}@2x.png`} alt="system icon"></img>
                         <h3>{item.dt_txt}</h3>
                         <h4>{item.main.temp}°C</h4>
                     </div>
--- src/components/CardWeather.jsx
+++ src/components/CardWeather.jsx
@@ -1,10 +1,11 @@
 
 const CardWeather = ({weather}) => {
-    const { name, main: { temp }, weather: [ { description } ] } = weather;
-    
+    const { name, main: { temp }, weather: [ { description, icon } ] } = weather;
+
     return (
         <div>
             <h2>Weather in: {name}</h2>
+            <img src={`https://openweathermap.org/img/wn/${icon}@2x.png`} alt="system icon"></img>
             <h3>{temp}°C</h3>
             <h4>{description}</h4>
         </div>
